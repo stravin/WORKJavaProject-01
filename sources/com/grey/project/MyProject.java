283dfaccaf18a5df74ebdf624ba375ef182962fe
Sergey Travin <sergey.a.travin@gmail.com>
2013-12-08 13:34:00 +0600

    Add a comment for the MyProject class

--- sources/com/grey/project/MyProject.java
+++ sources/com/grey/project/MyProject.java
@@ -1,5 +1,10 @@
 package com.grey.project;
 
+/**
+ * The class <code>MyProject</code> is a main
+ * class in the project that intended to learn
+ * the Java language.
+ */
 public class MyProject {
     public static void main(String[] args) {
         System.out.println("It is MyProject!");
